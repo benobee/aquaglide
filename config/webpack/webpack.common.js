6c3548bc094c754ca98db8dcd75891f247795a82
Ben Obee <benobee@gmail.com>
2019-02-19 21:35:49 -0800

    config change

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge');
+const merge = require("webpack-merge");
 const rules = require("./util/rules.js");
 const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -6,40 +6,40 @@ const MinifyPlugin = require("babel-minify-webpack-plugin");
 //const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = merge(rules, {
-    context: __dirname,
-    entry: ['../../main.js', '../../main.less'],
-    node: {
-        dns: 'mock',
-        net: 'mock',
-        fs: 'empty'
-    },
-    resolve: {
-        alias: {
-            'masonry': 'masonry-layout',
-            'isotope': 'isotope-layout',
-            'vue': 'vue/dist/vue.min.js'
-        }
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "bundle.css",
-            chunkFilename: "[id].css"
-        }),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery"
-        }),
-        new webpack.ProvidePlugin({
-            'Promise': 'es6-promise',
-            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-        }),
-        //new HtmlWebpackPlugin()
-    ],
-    output: {
-        publicPath: '/',
-        path: __dirname + "../../../template/assets",
-        filename: "bundle.js",
-        chunkFilename: "[name].bundle.js"
-    }
+	context: __dirname,
+	entry: ["../../main.js", "../../main.less"],
+	node: {
+		dns: "mock",
+		net: "mock",
+		fs: "empty"
+	},
+	resolve: {
+		alias: {
+			masonry: "masonry-layout",
+			isotope: "isotope-layout",
+			vue: "vue/dist/vue.min.js"
+		}
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "bundle.css",
+			chunkFilename: "[id].css"
+		}),
+		new webpack.ProvidePlugin({
+			"$": "jquery",
+			"jQuery": "jquery",
+			"window.jQuery": "jquery"
+		}),
+		new webpack.ProvidePlugin({
+			Promise: "es6-promise",
+			fetch: "imports?this=>global!exports?global.fetch!whatwg-fetch"
+		})
+		//new HtmlWebpackPlugin()
+	],
+	output: {
+		publicPath: "/",
+		path: `${__dirname }../../../../template/assets`,
+		filename: "bundle.js",
+		chunkFilename: "[name].bundle.js"
+	}
 });
